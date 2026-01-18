441d171f6ea8ac960eddb69d1e2938131c9fd07d
leonardo <366563421@qq.com>
2026-01-18 13:52:30 +0800

    feature:repair Usart2 to Usart1,PC10\PC11 to PA9\PA10

--- Dawn/DawnBsp/usart.c
+++ Dawn/DawnBsp/usart.c
@@ -41,40 +41,40 @@ void DebugUsartInit(uint32_t baudrate)
 {
     dma_single_data_parameter_struct dma_init_struct;
     
-    rcu_periph_clock_enable(RCU_GPIOC);
-    rcu_periph_clock_enable(RCU_USART2);
-    rcu_periph_clock_enable(RCU_DMA0);
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_USART0);
+    rcu_periph_clock_enable(RCU_DMA1);
     
-    gpio_af_set(GPIOC, GPIO_AF_7, GPIO_PIN_10);  
-    gpio_af_set(GPIOC, GPIO_AF_7, GPIO_PIN_11);  
+    gpio_af_set(GPIOA, GPIO_AF_7, GPIO_PIN_9);  
+    gpio_af_set(GPIOA, GPIO_AF_7, GPIO_PIN_10);  
     
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_10);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
+    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_9);
+    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
 
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_11);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_11);
+    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_10);
+    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
     
     
-    usart_deinit(USART2);
-    usart_baudrate_set(USART2, baudrate);
-    usart_word_length_set(USART2, USART_WL_8BIT);
-    usart_stop_bit_set(USART2, USART_STB_1BIT);
-    usart_parity_config(USART2, USART_PM_NONE);
-    usart_hardware_flow_rts_config(USART2, USART_RTS_DISABLE);
-    usart_hardware_flow_cts_config(USART2, USART_CTS_DISABLE);
-    usart_receive_config(USART2, USART_RECEIVE_ENABLE);
-    usart_transmit_config(USART2, USART_TRANSMIT_ENABLE);
+    usart_deinit(USART0);
+    usart_baudrate_set(USART0, baudrate);
+    usart_word_length_set(USART0, USART_WL_8BIT);
+    usart_stop_bit_set(USART0, USART_STB_1BIT);
+    usart_parity_config(USART0, USART_PM_NONE);
+    usart_hardware_flow_rts_config(USART0, USART_RTS_DISABLE);
+    usart_hardware_flow_cts_config(USART0, USART_CTS_DISABLE);
+    usart_receive_config(USART0, USART_RECEIVE_ENABLE);
+    usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
     
-    nvic_irq_enable(USART2_IRQn, DEBUG_USART_PRE_PRIORITY, DEBUG_USART_SUB_PRIORITY);
+    nvic_irq_enable(USART0_IRQn, DEBUG_USART_PRE_PRIORITY, DEBUG_USART_SUB_PRIORITY);
     
-    usart_dma_receive_config(USART2, USART_DENR_ENABLE);
-    usart_dma_transmit_config(USART2, USART_DENT_ENABLE);
+    usart_dma_receive_config(USART0, USART_DENR_ENABLE);
+    usart_dma_transmit_config(USART0, USART_DENT_ENABLE);
     
-    usart_flag_clear(USART2, USART_FLAG_TC);
-    usart_interrupt_enable(USART2, USART_INTEN_IDLEIE);    
+    usart_flag_clear(USART0, USART_FLAG_TC);
+    usart_interrupt_enable(USART0, USART_INTEN_IDLEIE);    
     
-    dma_deinit(DMA0, DMA_CH1);   
-    dma_init_struct.periph_addr = (uint32_t)&USART_DATA(USART2);
+    dma_deinit(DMA1, DMA_CH5);   
+    dma_init_struct.periph_addr = (uint32_t)&USART_DATA(USART0);
     dma_init_struct.memory0_addr = (uint32_t)&debug_usart_rx_buf[0];
     dma_init_struct.direction = DMA_PERIPH_TO_MEMORY;
     dma_init_struct.number = DEBUG_USART_RX_DMA_BUFFER_SIZE;
@@ -83,15 +83,15 @@ void DebugUsartInit(uint32_t baudrate)
     dma_init_struct.periph_memory_width = DMA_PERIPH_WIDTH_8BIT;
     dma_init_struct.circular_mode = DMA_CIRCULAR_MODE_DISABLE;
     dma_init_struct.priority = DMA_PRIORITY_ULTRA_HIGH;
-    dma_single_data_mode_init(DMA0, DMA_CH1, dma_init_struct);
-    dma_channel_subperipheral_select(DMA0, DMA_CH1, DMA_SUBPERI4);
+    dma_single_data_mode_init(DMA1, DMA_CH5, dma_init_struct);
+    dma_channel_subperipheral_select(DMA1, DMA_CH5, DMA_SUBPERI4);
     
     debug_usart_rx_dma_cnt = DEBUG_USART_RX_DMA_BUFFER_SIZE;
     
-    nvic_irq_enable(DMA0_Channel1_IRQn, DEBUG_USART_PRE_PRIORITY, DEBUG_USART_SUB_PRIORITY);
+    nvic_irq_enable(DMA1_Channel5_IRQn, DEBUG_USART_PRE_PRIORITY, DEBUG_USART_SUB_PRIORITY);
     
-    dma_deinit(DMA0, DMA_CH3);   
-    dma_init_struct.periph_addr = (uint32_t)&USART_DATA(USART2);
+    dma_deinit(DMA1, DMA_CH7);   
+    dma_init_struct.periph_addr = (uint32_t)&USART_DATA(USART0);
     dma_init_struct.memory0_addr = (uint32_t)&debug_usart_tx_buf[0];
     dma_init_struct.direction = DMA_MEMORY_TO_PERIPH;
     dma_init_struct.number = DEBUG_USART_TX_BUFF_SIZE;
@@ -100,21 +100,21 @@ void DebugUsartInit(uint32_t baudrate)
     dma_init_struct.periph_memory_width = DMA_PERIPH_WIDTH_8BIT;
     dma_init_struct.circular_mode = DMA_CIRCULAR_MODE_DISABLE;
     dma_init_struct.priority = DMA_PRIORITY_ULTRA_HIGH;
-    dma_single_data_mode_init(DMA0, DMA_CH3, dma_init_struct);
-    dma_channel_subperipheral_select(DMA0, DMA_CH3, DMA_SUBPERI4);
+    dma_single_data_mode_init(DMA1, DMA_CH7, dma_init_struct);
+    dma_channel_subperipheral_select(DMA1, DMA_CH7, DMA_SUBPERI4);
 
-    nvic_irq_enable(DMA0_Channel3_IRQn, DEBUG_USART_PRE_PRIORITY, DEBUG_USART_SUB_PRIORITY);
+    nvic_irq_enable(DMA1_Channel7_IRQn, DEBUG_USART_PRE_PRIORITY, DEBUG_USART_SUB_PRIORITY);
 
-    dma_channel_enable(DMA0, DMA_CH1);
-    dma_channel_disable(DMA0, DMA_CH3);          
+    dma_channel_enable(DMA1, DMA_CH5);
+    dma_channel_disable(DMA1, DMA_CH7);          
     
-    dma_interrupt_flag_clear(DMA0, DMA_CH1, DMA_INTC_FTFIFC);
-    dma_interrupt_flag_clear(DMA0, DMA_CH3, DMA_INTC_FTFIFC);
+    dma_interrupt_flag_clear(DMA1, DMA_CH5, DMA_INTC_FTFIFC);
+    dma_interrupt_flag_clear(DMA1, DMA_CH7, DMA_INTC_FTFIFC);
     
-    dma_interrupt_enable(DMA0, DMA_CH1, DMA_CHXCTL_FTFIE);
-    dma_interrupt_enable(DMA0, DMA_CH3, DMA_CHXCTL_FTFIE);
+    dma_interrupt_enable(DMA1, DMA_CH5, DMA_CHXCTL_FTFIE);
+    dma_interrupt_enable(DMA1, DMA_CH7, DMA_CHXCTL_FTFIE);
         
-    usart_enable(USART2);
+    usart_enable(USART0);
 }
 
 void debug_usart_trans_enable(void)
@@ -122,7 +122,7 @@ void debug_usart_trans_enable(void)
     
     if(is_debug_usart_dma_trans_completed == TRUE)
     {
-        usart_interrupt_enable(USART2, USART_INTEN_TBEIE);   
+        usart_interrupt_enable(USART0, USART_INTEN_TBEIE);   
     }
 }
 
@@ -175,22 +175,22 @@ void debug_usart_rx_buff_reset(void)
     debug_usart_rx_buff_tail_last = 0;
     debug_usart_rx_dma_cnt = DEBUG_USART_RX_DMA_BUFFER_SIZE;
     
-    dma_interrupt_disable(DMA0, DMA_CH1, DMA_CHXCTL_FTFIE);
-    dma_channel_disable(DMA0, DMA_CH1);
-    DMA_CHM0ADDR(DMA0, DMA_CH1) = (uint32_t)&debug_usart_rx_buf[debug_usart_rx_buff_head];
-    DMA_CHCNT(DMA0, DMA_CH1) = debug_usart_rx_dma_cnt;
+    dma_interrupt_disable(DMA1, DMA_CH5, DMA_CHXCTL_FTFIE);
+    dma_channel_disable(DMA1, DMA_CH5);
+    DMA_CHM0ADDR(DMA1, DMA_CH5) = (uint32_t)&debug_usart_rx_buf[debug_usart_rx_buff_head];
+    DMA_CHCNT(DMA1, DMA_CH5) = debug_usart_rx_dma_cnt;
     
-    dma_interrupt_flag_clear(DMA0, DMA_CH1, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
-    dma_channel_enable(DMA0, DMA_CH1);
-    dma_interrupt_enable(DMA0, DMA_CH1, DMA_CHXCTL_FTFIE);
+    dma_interrupt_flag_clear(DMA1, DMA_CH5, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
+    dma_channel_enable(DMA1, DMA_CH5);
+    dma_interrupt_enable(DMA1, DMA_CH5, DMA_CHXCTL_FTFIE);
 }
 
-void DMA0_Channel1_IRQHandler(void)
+void DMA1_Channel5_IRQHandler(void)
 {
     
-    if(dma_interrupt_flag_get(DMA0, DMA_CH1, DMA_INTF_FTFIF) == SET)
+    if(dma_interrupt_flag_get(DMA1, DMA_CH5, DMA_INTF_FTFIF) == SET)
     {
-        dma_interrupt_flag_clear(DMA0, DMA_CH1, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
+        dma_interrupt_flag_clear(DMA1, DMA_CH5, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
         
         if(debug_usart_rx_buff_head >= debug_usart_rx_buff_tail_last)   
         {
@@ -223,19 +223,19 @@ void DMA0_Channel1_IRQHandler(void)
             debug_usart_rx_dma_cnt = DEBUG_USART_RX_DMA_BUFFER_SIZE;
         }
 
-        dma_channel_disable(DMA0, DMA_CH1);
-        DMA_CHM0ADDR(DMA0, DMA_CH1) = (uint32_t)&debug_usart_rx_buf[debug_usart_rx_buff_head];
-        DMA_CHCNT(DMA0, DMA_CH1) = debug_usart_rx_dma_cnt;
-        dma_channel_enable(DMA0, DMA_CH1);
+        dma_channel_disable(DMA1, DMA_CH5);
+        DMA_CHM0ADDR(DMA1, DMA_CH5) = (uint32_t)&debug_usart_rx_buf[debug_usart_rx_buff_head];
+        DMA_CHCNT(DMA1, DMA_CH5) = debug_usart_rx_dma_cnt;
+        dma_channel_enable(DMA1, DMA_CH5);
     }
 }
 
-void DMA0_Channel3_IRQHandler(void)
+void DMA1_Channel7_IRQHandler(void)
 {
     
-    if(dma_interrupt_flag_get(DMA0, DMA_CH3, DMA_INTF_FTFIF) == SET)
+    if(dma_interrupt_flag_get(DMA1, DMA_CH7, DMA_INTF_FTFIF) == SET)
     {
-        dma_interrupt_flag_clear(DMA0, DMA_CH3, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
+        dma_interrupt_flag_clear(DMA1, DMA_CH7, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
 
         is_debug_usart_dma_trans_completed = TRUE;
         
@@ -243,17 +243,17 @@ void DMA0_Channel3_IRQHandler(void)
     }
 }
 
-void USART2_IRQHandler(void)
+void USART0_IRQHandler(void)
 {
     uint16_t len = 0;
     
     
-    if(usart_interrupt_flag_get(USART2, USART_INT_IDLEIE) == SET)   
+    if(usart_interrupt_flag_get(USART0, USART_INT_IDLEIE) == SET)   
     {
         
-        usart_data_receive(USART2);
+        usart_data_receive(USART0);
         
-        len = debug_usart_rx_dma_cnt - dma_transfer_number_get(DMA0, DMA_CH1);  
+        len = debug_usart_rx_dma_cnt - dma_transfer_number_get(DMA1, DMA_CH5);  
         debug_usart_rx_buff_head += len;
         
         debug_usart_rx_buff_tail_last = debug_usart_rx_buff_tail;
@@ -272,18 +272,18 @@ void USART2_IRQHandler(void)
             debug_usart_rx_dma_cnt = DEBUG_USART_RX_DMA_BUFFER_SIZE;
         }
 
-        dma_interrupt_disable(DMA0, DMA_CH1, DMA_CHXCTL_FTFIE);
-        dma_channel_disable(DMA0, DMA_CH1);
-        DMA_CHM0ADDR(DMA0, DMA_CH1) = (uint32_t)&debug_usart_rx_buf[debug_usart_rx_buff_head];
-        DMA_CHCNT(DMA0, DMA_CH1) = debug_usart_rx_dma_cnt;
+        dma_interrupt_disable(DMA1, DMA_CH5, DMA_CHXCTL_FTFIE);
+        dma_channel_disable(DMA1, DMA_CH5);
+        DMA_CHM0ADDR(DMA1, DMA_CH5) = (uint32_t)&debug_usart_rx_buf[debug_usart_rx_buff_head];
+        DMA_CHCNT(DMA1, DMA_CH5) = debug_usart_rx_dma_cnt;
         
-        dma_interrupt_flag_clear(DMA0, DMA_CH1, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
-        dma_channel_enable(DMA0, DMA_CH1);
-        dma_interrupt_enable(DMA0, DMA_CH1, DMA_CHXCTL_FTFIE);
+        dma_interrupt_flag_clear(DMA1, DMA_CH5, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
+        dma_channel_enable(DMA1, DMA_CH5);
+        dma_interrupt_enable(DMA1, DMA_CH5, DMA_CHXCTL_FTFIE);
     }
     
     
-    if(usart_interrupt_flag_get(USART2, USART_INT_TBEIE) == SET)
+    if(usart_interrupt_flag_get(USART0, USART_INT_TBEIE) == SET)
     {
         if(debug_usart_tx_buff_head > debug_usart_tx_buff_tail)       
         {
@@ -292,14 +292,14 @@ void USART2_IRQHandler(void)
             {
                 is_debug_usart_dma_trans_completed = FALSE;
                 
-                dma_interrupt_disable(DMA0, DMA_CH3, DMA_CHXCTL_FTFIE);
-                dma_channel_disable(DMA0, DMA_CH3);
-                DMA_CHM0ADDR(DMA0, DMA_CH3) = (uint32_t)&debug_usart_tx_buf[debug_usart_tx_buff_tail];
-                DMA_CHCNT(DMA0, DMA_CH3) = debug_usart_tx_buff_head - debug_usart_tx_buff_tail;
+                dma_interrupt_disable(DMA1, DMA_CH7, DMA_CHXCTL_FTFIE);
+                dma_channel_disable(DMA1, DMA_CH7);
+                DMA_CHM0ADDR(DMA1, DMA_CH7) = (uint32_t)&debug_usart_tx_buf[debug_usart_tx_buff_tail];
+                DMA_CHCNT(DMA1, DMA_CH7) = debug_usart_tx_buff_head - debug_usart_tx_buff_tail;
                 
-                dma_interrupt_flag_clear(DMA0, DMA_CH3, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
-                dma_channel_enable(DMA0, DMA_CH3);
-                dma_interrupt_enable(DMA0, DMA_CH3, DMA_CHXCTL_FTFIE);
+                dma_interrupt_flag_clear(DMA1, DMA_CH7, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
+                dma_channel_enable(DMA1, DMA_CH7);
+                dma_interrupt_enable(DMA1, DMA_CH7, DMA_CHXCTL_FTFIE);
                 
                 debug_usart_tx_buff_tail = debug_usart_tx_buff_head;
             }
@@ -311,24 +311,24 @@ void USART2_IRQHandler(void)
             {
                 is_debug_usart_dma_trans_completed = FALSE;
                 
-                dma_channel_disable(DMA0, DMA_CH3);
-                DMA_CHM0ADDR(DMA0, DMA_CH3) = (uint32_t)&debug_usart_tx_buf[debug_usart_tx_buff_tail];
-                DMA_CHCNT(DMA0, DMA_CH3) = DEBUG_USART_TX_BUFF_SIZE - debug_usart_tx_buff_tail;
+                dma_channel_disable(DMA1, DMA_CH7);
+                DMA_CHM0ADDR(DMA1, DMA_CH7) = (uint32_t)&debug_usart_tx_buf[debug_usart_tx_buff_tail];
+                DMA_CHCNT(DMA1, DMA_CH7) = DEBUG_USART_TX_BUFF_SIZE - debug_usart_tx_buff_tail;
                 
-                dma_interrupt_flag_clear(DMA0, DMA_CH3, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
-                dma_channel_enable(DMA0, DMA_CH3);
-                dma_interrupt_enable(DMA0, DMA_CH3, DMA_CHXCTL_FTFIE);
+                dma_interrupt_flag_clear(DMA1, DMA_CH7, DMA_INTC_FTFIFC | DMA_INTC_HTFIFC);
+                dma_channel_enable(DMA1, DMA_CH7);
+                dma_interrupt_enable(DMA1, DMA_CH7, DMA_CHXCTL_FTFIE);
                 
                 debug_usart_tx_buff_tail = 0;
             }
         }
-        usart_interrupt_disable(USART2, USART_INTEN_TBEIE);
+        usart_interrupt_disable(USART0, USART_INTEN_TBEIE);
     }
     
     
-    if(usart_flag_get(USART2, USART_FLAG_ORERR) == SET)
+    if(usart_flag_get(USART0, USART_FLAG_ORERR) == SET)
     {
-        usart_data_receive(USART2);
-        usart_flag_clear(USART2, USART_FLAG_ORERR);
+        usart_data_receive(USART0);
+        usart_flag_clear(USART0, USART_FLAG_ORERR);
     }
 }
